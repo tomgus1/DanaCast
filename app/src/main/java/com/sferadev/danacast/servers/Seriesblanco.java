7f8f2070554354671b6e719ca9f7c8ea1820e374
Alexis Rico <me@sferadev.com>
2015-08-23 14:51:20 +0200

    Dana: Add initial series blanco server

---
+++ app/src/main/java/com/sferadev/danacast/servers/Seriesblanco.java
@@ -0,0 +1,32 @@
+package com.sferadev.danacast.servers;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class Seriesblanco {
+    public static Elements getSearchResults(String query) {
+        try {
+            Document document = Jsoup.connect("http://seriesblanco.com/finder.php")
+                    .data("query", query)
+                    .post();
+            return document.select("a");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Elements getPopularContent() {
+        try {
+            Document document = Jsoup.connect("http://www.seriesblanco.com/")
+                    .get();
+            return document.select("#PopularPosts1 li");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
