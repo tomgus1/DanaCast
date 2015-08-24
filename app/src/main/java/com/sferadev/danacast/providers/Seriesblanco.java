cd517ccdcad587c4083fdfbc3994b643a7ddb09a
Alexis Rico <me@sferadev.com>
2015-08-24 10:06:45 +0200

    Dana: SeriesBlanco getEpisodes

--- app/src/main/java/com/sferadev/danacast/providers/Seriesblanco.java
+++ app/src/main/java/com/sferadev/danacast/providers/Seriesblanco.java
@@ -17,9 +17,10 @@ public class Seriesblanco {
             @Override
             public void run() {
                 try {
-                    Document document = Jsoup.connect("http://seriesblanco.com/search.php" + "?q1=" + query.replace(" ", "+"))
-                            .get();
-                    Elements elements = document.getElementsByClass("post-header").first().getElementsByTag("a");
+                    Document document = Jsoup.connect("http://seriesblanco.com/search.php" +
+                            "?q1=" + query.replace(" ", "+")).get();
+                    Elements elements = document.getElementsByClass("post-header").first()
+                            .getElementsByTag("a");
                     for (Element element : elements) {
                         if (element.hasText()) {
                             String title = element.text();
@@ -48,14 +49,46 @@ public class Seriesblanco {
             @Override
             public void run() {
                 try {
-                    Document document = Jsoup.connect("http://www.seriesblanco.com/")
-                            .get();
-                    Elements elements = document.select("#PopularPosts1").first().getElementsByTag("li");
+                    Document document = Jsoup.connect("http://www.seriesblanco.com/").get();
+                    Elements elements = document.select("#PopularPosts1").first()
+                            .getElementsByTag("li");
                     for (Element element : elements) {
                         String title = element.getElementsByTag("img").first().attr("title");
                         String url = element.getElementsByTag("a").first().attr("abs:href");
                         String pic = element.getElementsByTag("img").first().attr("src");
                         result.add(new EntryModel(title, url, pic));
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+        try {
+            thread.join();
+            return result;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static ArrayList<EntryModel> getEpisodeList(final String url) {
+        final ArrayList<EntryModel> result = new ArrayList<>();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Document document = Jsoup.connect(url).get();
+                    Elements elements = document.getElementsByClass("zebra").first()
+                            .getElementsByTag("tbody").first()
+                            .getElementsByTag("a");
+                    for (Element element : elements) {
+                        if (element.hasText()) {
+                            String title = element.text();
+                            String url = element.attr("abs:href");
+                            result.add(new EntryModel(title, url, null));
+                        }
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
