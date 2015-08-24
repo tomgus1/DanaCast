a39dd958da6bde231e989f1a92bd858d1a714fbb
Alexis Rico <me@sferadev.com>
2015-08-24 09:22:52 +0200

    Dana: Fix Nowvideo to work

--- app/src/main/java/com/sferadev/danacast/servers/Nowvideo.java
+++ app/src/main/java/com/sferadev/danacast/servers/Nowvideo.java
@@ -4,16 +4,29 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 public class Nowvideo {
-    public String getVideoPath(String url) {
+    public static String getVideoPath(final String url) {
+        final String[] response = {null};
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String id = url.substring(url.lastIndexOf("/") + 1);
+                    Document doc = Jsoup.connect("http://www.nowvideo.sx/mobile/video.php?id=" + id)
+                            .timeout(3000)
+                            .get();
+                    response[0] = doc.select("source[type=video/mp4]").attr("src");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
         try {
-            String id = url.substring(url.lastIndexOf("/") + 1);
-            Document doc = Jsoup.connect("http://www.nowvideo.sx/mobile/video.php?id=" + id)
-                    .timeout(3000)
-                    .get();
-            return doc.select("source[type=video/mp4]").attr("src");
-        } catch (Exception e) {
+            thread.join();
+            return response[0];
+        } catch (InterruptedException e) {
             e.printStackTrace();
+            return null;
         }
-        return null;
     }
 }
