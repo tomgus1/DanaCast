fbfe851b8fdcbf946dd51fd3b967ee3ebe6b3317
Alexis Rico <me@sferadev.com>
2015-08-25 09:07:27 +0200

    Dana: Add support for direct files

--- app/src/main/java/com/sferadev/danacast/servers/Server.java
+++ app/src/main/java/com/sferadev/danacast/servers/Server.java
@@ -3,9 +3,15 @@ package com.sferadev.danacast.servers;
 public class Server {
     public static String getVideoPath(String url) {
         try {
-            if (url.contains("streamcloud")) return Streamcloud.getVideoPath(url);
-            if (url.contains("nowvideo")) return Nowvideo.getVideoPath(url);
-            if (url.contains("rocvideo")) return Rocvideo.getVideoPath(url);
+            if (url.contains("streamcloud")) {
+                return Streamcloud.getVideoPath(url);
+            } else if (url.contains("nowvideo")) {
+                return Nowvideo.getVideoPath(url);
+            } else if (url.contains("rocvideo")) {
+                return Rocvideo.getVideoPath(url);
+            } else return url.contains(".") && url.substring(0,
+                    url.lastIndexOf('.')).equals("mp4")
+                    ? url : null;
         } catch (NullPointerException e) {
             e.printStackTrace();
         }
