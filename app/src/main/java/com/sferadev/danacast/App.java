70ac8838998d702f25097816430e179d79631948
Alexis Rico <me@sferadev.com>
2015-08-25 16:53:51 +0200

    Dana: Add auto-update framework

--- app/src/main/java/com/sferadev/danacast/App.java
+++ app/src/main/java/com/sferadev/danacast/App.java
@@ -2,6 +2,10 @@ package com.sferadev.danacast;
 
 import android.support.multidex.MultiDexApplication;
 
+import com.github.snowdream.android.app.UpdateFormat;
+import com.github.snowdream.android.app.UpdateManager;
+import com.github.snowdream.android.app.UpdateOptions;
+import com.github.snowdream.android.app.UpdatePeriod;
 import com.google.android.libraries.cast.companionlibrary.cast.VideoCastManager;
 import com.google.android.libraries.cast.companionlibrary.cast.player.VideoCastControllerActivity;
 
@@ -18,6 +22,16 @@ public class App extends MultiDexApplication {
                         VideoCastManager.FEATURE_LOCKSCREEN |
                         VideoCastManager.FEATURE_WIFI_RECONNECT |
                         VideoCastManager.FEATURE_DEBUGGING);
+
+        UpdateManager manager = new UpdateManager(this);
+
+        UpdateOptions options = new UpdateOptions.Builder(this)
+                .checkUrl("https://raw.githubusercontent.com/SferaDev/DanaCast/master/updates/update.json")
+                .updateFormat(UpdateFormat.JSON)
+                .updatePeriod(new UpdatePeriod(UpdatePeriod.EACH_TIME))
+                .checkPackageName(true)
+                .build();
+        manager.check(this, options);
     }
 
 }
