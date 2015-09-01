ba22c843da7882dd2ca524e9f20026a283c636b4
Alexis Rico <me@sferadev.com>
2015-09-01 16:11:26 +0200

    Dana: Add application context

--- app/src/main/java/com/sferadev/danacast/App.java
+++ app/src/main/java/com/sferadev/danacast/App.java
@@ -1,15 +1,23 @@
 package com.sferadev.danacast;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.google.android.libraries.cast.companionlibrary.cast.VideoCastManager;
 import com.google.android.libraries.cast.companionlibrary.cast.player.VideoCastControllerActivity;
 
 public class App extends Application {
 
+    private static Context mContext;
+
+    public static Context getContext() {
+        return mContext;
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
+        mContext = this;
         String applicationId = getString(R.string.app_id);
 
         VideoCastManager.
